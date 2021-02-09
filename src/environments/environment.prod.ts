f4d54db9559e5c6b6c1522341fe79057d8ecca44
Peerapat Surasing <luepeeco@gmail.com>
2021-02-09 21:24:57 +0700

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyESernsZ4vMmSyI4dq3rOR8U48s9N5sA",
+    authDomain: "qr-quick-order.firebaseapp.com",
+    databaseURL: "https://qr-quick-order.firebaseio.com",
+    projectId: "qr-quick-order",
+    storageBucket: "qr-quick-order.appspot.com",
+    messagingSenderId: "866086818069",
+    appId: "1:866086818069:web:45211da1fa5528d659d91c",
+    measurementId: "G-WYN1KCWZ8M"
+  }
 };
